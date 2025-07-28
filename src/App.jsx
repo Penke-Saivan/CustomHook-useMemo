fc90baf2465bba5568a9e102e2f29841fab70919
SaiPavan-Penke <pavantonystark99@gmail.com>
2025-07-28 22:38:22 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  function handleCLick() {
+    setCount(count + 1);
+  }
+  function double() {
+    console.log("Double");
+
+    return count * count;
+  }
+
+  return (
+    <>
+      <h1>Counter1: {count}</h1>
+      <h2>Double:{double()}</h2>
+      <button type="button" onClick={handleCLick}>
+        Click{" "}
+      </button>
+      <h2>Counter2: {count1}</h2>
+      <button
+        onClick={() => {
+          setCount1(count1 - 1);
+        }}
+      >
+        decrement
+      </button>
+    </>
+  );
+}
+
+export default App;
